dba94ba8f7ed85f9bb3bae635d7d997d6e048095
kvkvkvkvkv <kvsreenidhi@gmail.com>
2025-05-25 17:58:29 +0530

    comparison

--- src/main/java/org/example/singleton/ConfigManagerEnum.java
+++ src/main/java/org/example/singleton/ConfigManagerEnum.java
@@ -5,6 +5,36 @@ package org.example.singleton;
 //can't extend another class as enum extend Enum
 //Spring preferes class based singleton
 //always eager init, not lazy
+/*
+Eager Initialization
+Instance created at class load time.
+
+Simple and thread-safe.
+
+        Drawback: instance created even if never used.
+
+        2. Lazy Initialization (Non-thread-safe)
+Instance created only when needed.
+
+Not safe in multithreaded environment.
+
+Risk of multiple instances if accessed concurrently.
+
+        3. Lazy Initialization with Synchronization
+Thread-safe lazy initialization.
+
+Synchronization adds overhead.
+
+Can be optimized using Double-Checked Locking for better performance.
+
+        4. Enum Singleton
+Simplest and safest approach.
+
+Handles serialization and reflection attacks internally.
+
+Does not support lazy loading.
+
+Cannot extend other classes (enums extend java.lang.Enum).*/
 public enum ConfigManagerEnum {
     INSTANCE;
 
